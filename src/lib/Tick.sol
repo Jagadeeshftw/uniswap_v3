dd9626bb59ead3c1bec51dfdf93ced4c85ebc09b
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-10-03 00:25:07 +0530

    implement tickSpacingToMaxLiquidityPerTick

--- src/lib/Tick.sol
+++ src/lib/Tick.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.19;
 
-library Tick {
-        function tickSpacingToMaxLiquidityPerTick(int24 _tickSpacing) internal pure returns (uint128)
-        {
-            //TODO:
-        }
+import {TickMath} from "../lib/TickMath.sol";
 
+library Tick {
+    function tickSpacingToMaxLiquidityPerTick(int24 tickSpacing) internal pure returns (uint128) {
+        int24 minTick = (TickMath.MIN_TICK / tickSpacing) * tickSpacing;
+        int24 maxTick = (TickMath.MAX_TICK / tickSpacing) * tickSpacing;
+        uint24 numTicks = uint24((maxTick - minTick) / tickSpacing) + 1;
+        return type(uint128).max / numTicks;
+    }
 }
