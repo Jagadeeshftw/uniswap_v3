2382f3d07ea0b34addc6b668bfa9f70827b43ee3
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-10-03 00:13:36 +0530

    Add Tick library

---
+++ src/lib/Tick.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.19;
+
+library Tick {
+        function tickSpacingToMaxLiquidityPerTick(int24 _tickSpacing) internal pure returns (uint128)
+        {
+            //TODO:
+        }
+
+}
