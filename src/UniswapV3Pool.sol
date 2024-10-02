ad8895df65189cbd5dac4f3de237d40c799575b5
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-10-03 01:06:03 +0530

    Add mint function

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -4,6 +4,8 @@ pragma solidity >=0.8.19;
 
 import {Tick} from "./lib/Tick.sol";
 import {TickMath} from "./lib/TickMath.sol";
+import {Position} from "./lib/Position.sol";
+import {SafeCast} from "./lib/SafeCast.sol";
 
 contract UniswapV3Pool {
     address public immutable factory;
@@ -12,6 +14,20 @@ contract UniswapV3Pool {
     uint24 public immutable fee;
     int24 public immutable tickSpacing;
     uint128 public immutable maxLiquidityPerTick;
+
+    using SafeCast for int256;
+
+    mapping(bytes32 => Position.Info) public positions;
+
+    /// @dev Mutually exclusive reentrancy protection into the pool to/from a method. This method also prevents entrance
+    /// to a function before the pool is initialized. The reentrancy guard is required throughout the contract because
+    /// we use balance checks to determine the payment status of interactions such as mint, swap and flash.
+    modifier lock() {
+        require(slot0.unlocked, "LOK");
+        slot0.unlocked = false;
+        _;
+        slot0.unlocked = true;
+    }
 
     struct Slot0 {
         // the current price
@@ -24,6 +40,28 @@ contract UniswapV3Pool {
 
     Slot0 public slot0;
 
+    struct ModifyPositionParams {
+        // the address that owns the position
+        address owner;
+        // the lower and upper tick of the position
+        int24 tickLower;
+        int24 tickUpper;
+        // any change in liquidity
+        int128 liquidityDelta;
+    }
+
+    /// @dev Effect some changes to a position
+    /// @param params the position details and the change to the position's liquidity to effect
+    /// @return position a storage pointer referencing the position with the given owner and tick range
+    /// @return amount0 the amount of token0 owed to the pool, negative if the pool should pay the recipient
+    /// @return amount1 the amount of token1 owed to the pool, negative if the pool should pay the recipient
+    function _modifyPosition(ModifyPositionParams memory params)
+        private
+        returns (Position.Info storage position, int256 amount0, int256 amount1)
+    {
+        return (positions[bytes32(0)], 0, 0);
+    }
+
     constructor(address _token0, address _token1, uint24 _fee, int24 _tickSpacing) {
         maxLiquidityPerTick = Tick.tickSpacingToMaxLiquidityPerTick(_tickSpacing);
     }
@@ -35,5 +73,25 @@ contract UniswapV3Pool {
         int24 tick = TickMath.getTickAtSqrtRatio(sqrtPriceX96);
 
         slot0 = Slot0({sqrtPriceX96: sqrtPriceX96, tick: tick, unlocked: true});
+    }
+
+    /// @dev noDelegateCall is applied indirectly via _modifyPosition
+    function mint(address recipient, int24 tickLower, int24 tickUpper, uint128 amount, bytes calldata data)
+        external
+        lock
+        returns (uint256 amount0, uint256 amount1)
+    {
+        require(amount > 0);
+        (, int256 amount0Int, int256 amount1Int) = _modifyPosition(
+            ModifyPositionParams({
+                owner: recipient,
+                tickLower: tickLower,
+                tickUpper: tickUpper,
+                liquidityDelta: int256(int128(amount)).toInt128()
+            })
+        );
+
+        amount0 = uint256(amount0Int);
+        amount1 = uint256(amount1Int);
     }
 }
